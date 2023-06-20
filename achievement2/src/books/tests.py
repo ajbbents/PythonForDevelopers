8d13e08b60723bd75f66271451301d502e706b2b
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-20 14:11:04 -0500

    add learning from 2.3

--- achievement2/src/books/tests.py
+++ achievement2/src/books/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Book  # to access the Book model
 
 # Create your tests here.
+
+
+class BookModelTest(TestCase):
+    def setUpTestData():
+        Book.objects.create(name='Wild', author_name='Cheryl Strayed',
+                            genre='educational', book_type='hardcover', price='25.03')
+
+    def test_book_name(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_author_name_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('author_name').max_length
+        self.assertEqual(max_length, 120)
--- achievement2/src/salespersons/models.py
+++ achievement2/src/salespersons/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User  # needed for the OneToOneField
 
 # Create your models here.
+
+
+class Salesperson(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="no bio... start one up!")
+
+    def __str__(self):
+        return f"Profile of {self.username}"
+        # f-string allows formatting of string
