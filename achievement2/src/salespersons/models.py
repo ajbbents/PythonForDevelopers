be5d0a0820e3c4e0976d61bb16bb021517d25a15
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-22 14:26:03 -0500

    adding images

--- achievement2/src/salespersons/models.py
+++ achievement2/src/salespersons/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User  # needed for the OneToOneField
 class Salesperson(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default="no bio... start one up!")
+    pic = models.ImageField(upload_to='salespersons', default='no_picture.jpg')
 
     def __str__(self):
         return f"Profile of {self.username}"
