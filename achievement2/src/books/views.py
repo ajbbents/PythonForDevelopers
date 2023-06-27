71a083a89ca66f0286cb288768570eba5f893b19
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-27 14:31:39 -0500

    working through 2.6 lesson

--- achievement2/src/books/views.py
+++ achievement2/src/books/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render  # default import
 from django.views.generic import ListView, DetailView  # to display lists, details
 from .models import Book  # access Book model
+# to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BookListView(ListView):  # CBV
+class BookListView(LoginRequiredMixin, ListView):  # CBV
     model = Book  # specifying model
     template_name = 'books\main.html'  # specifying template
 
 
-class BookDetailView(DetailView):  # CBV
+class BookDetailView(LoginRequiredMixin, DetailView):  # CBV
     model = Book  # specifying model again
     template_name = 'books\detail.html'  # specifying template again
--- achievement2/src/sales/views.py
+++ achievement2/src/sales/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'sales/home.html')
+
+
+# define function-based view: records
+@login_required
+def records(request):
+    return render(request, 'sales/records.html')
